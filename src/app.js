e4752aa701d861bc1049bc24ff09f97e4fc00274
Alexander Telyatnikov <labriko@yandex.ru>
2017-03-21 00:35:05 +0300

    Axios

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import welcome from './welcome';
 
 welcome('home');
 
+import axios from 'axios';
+
 const template = require('./template.tpl.html');
 const output = template({greeting: 'Hello World.'});
 console.log(output);
@@ -9,6 +11,26 @@ console.log(output);
 class TBWidget {
   constructor(settings) {
     this.settings = settings;
+    this.getData();
+  }
+
+  getData() {
+    console.log('getData');
+
+    const { type } = this.settings;
+    if (type === 'course') {
+      // Ajax
+      console.log('1');
+
+      axios.get('https://api.github.com/users/enrikoLabriko')
+        .then((res) => {
+          console.log(res);
+        });
+      debugger;
+    } else if (type === 'webinar') {
+      // Ajax
+      console.log('2');
+    }
   }
 }
 
