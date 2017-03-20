cb438846d92f439c929be47652317714f7651c89
Alexander Telyatnikov <labriko@yandex.ru>
2017-03-21 00:37:48 +0300

    Mock axios

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ import welcome from './welcome';
 welcome('home');
 
 import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
+const mock = new MockAdapter(axios);
+
+mock.onGet('/users').reply(200, {
+  users: [
+    { id: 1, name: 'John Smith' }
+  ]
+});
 
 const template = require('./template.tpl.html');
 const output = template({greeting: 'Hello World.'});
@@ -22,7 +31,7 @@ class TBWidget {
       // Ajax
       console.log('1');
 
-      axios.get('https://api.github.com/users/enrikoLabriko')
+      axios.get('/users')
         .then((res) => {
           console.log(res);
         });
