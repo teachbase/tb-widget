f0634948d67494def7400f4235623f28cdeea180
Alexander Telyatnikov <labriko@yandex.ru>
2017-03-22 13:17:21 +0300

    Some template refactor

--- src/app.js
+++ src/app.js
@@ -17,35 +17,34 @@ class TBWidget {
 
   mockApiResponses() {
     const mock = new MockAdapter(axios);
+    const { resourceId } = this.settings;
 
-    mock.onGet(`/courses/${this.settings.resourceId}`).reply(200, {
+    mock.onGet(`/courses/${resourceId}`).reply(200, {
       course: {
-        id: this.settings.resourceId,
+        id: resourceId,
+        type: 'course',
         title: 'Course title',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad, at beatae blanditiis, consequatur dicta error esse et hic impedit laboriosam laudantium maiores perspiciatis qui repellat rerum saepe sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus!',
         img: {
           src: 'http://cf2.teachbase.ru/system/products/course_sessions/1067/icons/small/845c6d7a765f5a30882d0bbe6118fac11107de59.jpeg',
-          alt: 'Иконка курса' // TODO: Вынести в шаблон с условием
         },
         button: {
-          text: 'Перейти к курсу', // TODO: Вынести в шаблон с условием
           url: 'http://go.teachbase.ru/stores/conditoria'
         }
       }
     });
 
-    mock.onGet(`/meeting/${this.settings.resourceId}`).reply(200, {
+    mock.onGet(`/meeting/${resourceId}`).reply(200, {
       meeting: {
-        id: this.settings.resourceId,
+        id: resourceId,
+        type: 'meeting',
         title: 'Meeting title',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad, at beatae blanditiis, consequatur dicta error esse et hic impedit laboriosam laudantium maiores perspiciatis qui repellat rerum saepe sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus!',
         host: 'Meeting host',
         img: {
           src: 'http://cf2.teachbase.ru/system/products/course_sessions/1067/icons/small/845c6d7a765f5a30882d0bbe6118fac11107de59.jpeg',
-          alt: 'Иконка курса' // TODO: Вынести в шаблон с условием
         },
         button: {
-          text: 'Перейти к курсу', // TODO: Вынести в шаблон с условием
           url: 'http://go.teachbase.ru/stores/conditoria'
         }
       }
@@ -71,14 +70,9 @@ class TBWidget {
   render(data) {
     const { orientation } = this.settings;
 
-    let r = Object.assign(data, { styles });
-    let output = template(r);
-
-    if (orientation === 'landscape') {
-      r = Object.assign(data, { styles: stylesLandscape });
-      console.log(r);
-      output = templateLandscape(r);
-    }
+    const output = orientation === 'landscape'
+      ? templateLandscape(Object.assign(data, { styles: stylesLandscape }))
+      : template(Object.assign(data, { styles }));
 
     document.getElementById('tb_widget').innerHTML = output;
   }
