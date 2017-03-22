1c0f52b73572395825b354b04113e1a74076abfc
Alexander Telyatnikov <labriko@yandex.ru>
2017-03-22 12:54:01 +0300

    Meeting version

--- src/app.js
+++ src/app.js
@@ -18,14 +18,14 @@ class TBWidget {
   mockApiResponses() {
     const mock = new MockAdapter(axios);
 
-    mock.onGet(`/courses/${this.settings.id}`).reply(200, {
+    mock.onGet(`/courses/${this.settings.resourceId}`).reply(200, {
       course: {
-        id: this.settings.id,
+        id: this.settings.resourceId,
         title: 'Course title',
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad, at beatae blanditiis, consequatur dicta error esse et hic impedit laboriosam laudantium maiores perspiciatis qui repellat rerum saepe sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus!',
         img: {
           src: 'http://cf2.teachbase.ru/system/products/course_sessions/1067/icons/small/845c6d7a765f5a30882d0bbe6118fac11107de59.jpeg',
-          alt: 'Иконка курса'
+          alt: 'Иконка курса' // TODO: Вынести в шаблон с условием
         },
         button: {
           text: 'Перейти к курсу', // TODO: Вынести в шаблон с условием
@@ -34,11 +34,20 @@ class TBWidget {
       }
     });
 
-    mock.onGet(`/webinars/${this.settings.id}`).reply(200, {
-      course: {
-        id: this.settings.id,
-        title: 'Webinar title',
-        description: 'Webinar description'
+    mock.onGet(`/meeting/${this.settings.resourceId}`).reply(200, {
+      meeting: {
+        id: this.settings.resourceId,
+        title: 'Meeting title',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad, at beatae blanditiis, consequatur dicta error esse et hic impedit laboriosam laudantium maiores perspiciatis qui repellat rerum saepe sunt ullam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi consectetur consequuntur dicta enim eos quibusdam quod reiciendis ullam. Corporis doloribus fugiat non provident voluptatum. Debitis dolorum labore laboriosam nostrum possimus!',
+        host: 'Meeting host',
+        img: {
+          src: 'http://cf2.teachbase.ru/system/products/course_sessions/1067/icons/small/845c6d7a765f5a30882d0bbe6118fac11107de59.jpeg',
+          alt: 'Иконка курса' // TODO: Вынести в шаблон с условием
+        },
+        button: {
+          text: 'Перейти к курсу', // TODO: Вынести в шаблон с условием
+          url: 'http://go.teachbase.ru/stores/conditoria'
+        }
       }
     });
   }
@@ -47,31 +56,25 @@ class TBWidget {
     const { type } = this.settings;
 
     if (type === 'course') {
-      // Ajax
-      console.log('1');
-
-      axios.get(`/courses/${this.settings.id}`)
+      axios.get(`/courses/${this.settings.resourceId}`)
         .then((res) => {
-          console.log(res);
           this.render(res.data.course);
         })
-    } else if (type === 'webinar') {
-      // Ajax
-      console.log('2');
+    } else if (type === 'meeting') {
+      axios.get(`/meeting/${this.settings.resourceId}`)
+        .then((res) => {
+          this.render(res.data.meeting)
+        })
     }
   }
 
   render(data) {
-
-    console.log('222');
     const { orientation } = this.settings;
 
     let r = Object.assign(data, { styles });
     let output = template(r);
 
     if (orientation === 'landscape') {
-      console.log('landscape');
-
       r = Object.assign(data, { styles: stylesLandscape });
       console.log(r);
       output = templateLandscape(r);
